cf8135094c5701dc04cde068f38d234fbd59368c
glopes <glopesdev@users.noreply.github.com>
2017-08-07 17:06:02 +0100

    Added first draft of Bonsai pylon interface.

---
+++ Bonsai.Pylon/CaptureException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Pylon
+{
+    [Serializable]
+    public class CaptureException : Exception
+    {
+        public CaptureException()
+        {
+        }
+
+        public CaptureException(string message)
+            : base(message)
+        {
+        }
+
+        public CaptureException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected CaptureException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
